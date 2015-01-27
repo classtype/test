61d781efa9758a42ecf7ad023e906d2784d84134
classtype <classtype@mail.ru>
2015-01-27 14:37:50 +0000

    fix index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World11\n');
+    res.end('Hello World123\n');
 }).listen(8080, IP);
 
 console.log('Server running at http://'+IP+'/');
