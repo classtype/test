448835a3f6d45ef485a56a3052675dfeee52277e
classtype <classtype@mail.ru>
2015-01-28 09:17:18 +0000

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World123456\n');
+    res.end('Всем привет!\n');
 }).listen(8080, IP);
 
 console.log('Server running at http://'+IP+'/');
