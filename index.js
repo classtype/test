50f0a4daf8fc48394e6fcc0531c962ffaed1add7
classtype <classtype@mail.ru>
2015-01-28 10:43:53 +0000

    fix port

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Всем привет!\n');
-}).listen(8080, IP);
+}).listen(80, IP);
 
 console.log('Server running at http://'+IP+'/');
