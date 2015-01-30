119ba187a44ae8cbe48ed0cc1fd477288139c363
classtype <classtype@mail.ru>
2015-01-30 06:15:30 +0000

    001

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+var spawn = require('child_process').spawn;
+
+
+var child = spawn('ssh', ['-o', 'StrictHostKeyChecking=no', 'root@109.234.154.170']);
+
+child.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+
+
+child.stderr.on('data', function (data) {
+    child.stdin.write('g8p2g5mg');
+    console.log('stderr'+data);
+});
+
+
+child.stdin.on('data', function (data) {
+    console.log('stdin: ' + data);
+});
+
+
+child.on('close', function (code) {
+    console.log('close');
+});
+
+child.on('data', function (code) {
+    console.log('data');
+});
+
+
+/*
 var IP = require('os').networkInterfaces()['venet0:0'][0].address;
 var http = require('http');
 
@@ -7,3 +38,4 @@ http.createServer(function (req, res) {
 }).listen(80, IP);
 
 console.log('Server running at http://'+IP+'/');
+*/
